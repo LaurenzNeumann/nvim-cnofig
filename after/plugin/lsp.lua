a204cc39f0c4a01b90551cb35b753a7a98fae9c0
Laurenz Neumann <laurenz.neumann@dbis.rwth-aachen.de>
2024-10-14 15:13:13 +0200

    inserted explicit keymaps for lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,28 +1,35 @@
 local lsp_zero = require('lsp-zero')
+-- from lsp zero installation guide
+vim.opt.signcolumn = 'yes'
 
--- lsp_attach is where you enable features that only work
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require('lspconfig').util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lspconfig_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- This is where you enable features that only work
 -- if there is a language server active in the file
-local lsp_attach = function(client, bufnr)
-    lsp_zero.default_keymaps{buffer = bufnr, preserve_mappings=false}
-end
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
-lsp_zero.extend_lspconfig({
-  sign_text = true,
-  lsp_attach = lsp_attach,
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
 })
-
--- enable format on save
-lsp_zero.format_on_save({
-    format_opts = {
-        async=false,
-        timeout_ms=10000,
-    },
-    servers = {
-        ['rust_analyzer']={'rust'}
-    }
-})
-
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 -- from https://github.com/zbirenbaum/copilot-cmp
